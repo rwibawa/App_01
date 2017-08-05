bb11d826a133d0ec4b6b17c475a737132dc5fbf8
Wibawa, Ryan (GE Digital, consultant) <ryan.wibawa@ge.com>
2017-08-04 23:15:35 -0700

    Add Internationalization.

--- app/src/main/java/com/navisow/android/app_01/MainActivity.java
+++ app/src/main/java/com/navisow/android/app_01/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a string from Resources
+        String hello = getResources().getString(R.string.hello_world);
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setText(hello);
     }
 
     // Send Button handler
