7337b9fd1bc3cae993918dd8f39115ab21daab47
Wibawa, Ryan (GE Digital, consultant) <ryan.wibawa@ge.com>
2017-08-09 09:58:54 -0700

    Interacting with other apps: sharing simple data.

--- README.md
+++ README.md
@@ -12,3 +12,87 @@ the send button. The event will instantiate DisplayMessageActivity and show the 
 
 ### DisplayMessageActivity
 It only contain a ViewText that shows the sent text.
+
+## 2. Internationalization
+Default is US, and alternative is ES (Spanish)
+```
+MyProject/
+    res/
+       values/
+           strings.xml
+       values-b+es/
+           strings.xml
+       mipmap/
+           country_flag.png
+       mipmap-b+es+ES/
+           country_flag.png
+```
+
+## 3. Supporting Different Screens
+* There are four generalized sizes: small, normal, large, xlarge
+* And four generalized densities: low (ldpi), medium (mdpi), high (hdpi), extra high (xhdpi)
+
+For example, this project includes a default layout and an alternative layout for large screens:
+```
+MyProject/
+    res/
+        layout/
+            main.xml
+        layout-large/
+            main.xml
+```
+
+As another example, here's a project with an alternative layout for landscape orientation:
+```
+MyProject/
+    res/
+        layout/
+            main.xml
+        layout-land/
+            main.xml
+```
+By default, the layout/main.xml file is used for portrait orientation.
+
+If you want to provide a special layout for landscape, including while on large screens, then you need to use both the large and land qualifier:
+```
+MyProject/
+    res/
+        layout/              # default (portrait)
+            main.xml
+        layout-land/         # landscape
+            main.xml
+        layout-large/        # large (portrait)
+            main.xml
+        layout-large-land/   # large landscape
+            main.xml
+```
+
+To generate these images, you should start with your raw resource in vector format and generate the images for each density using the following size scale:
+
+* xhdpi: 2.0
+* hdpi: 1.5
+* mdpi: 1.0 (baseline)
+* ldpi: 0.75
+
+This means that if you generate a 200x200 image for xhdpi devices, you should generate the same resource in 150x150 for hdpi, 100x100 for mdpi, and 75x75 for ldpi devices.
+
+Then, place the files in the appropriate drawable resource directory:
+```
+MyProject/
+    res/
+        drawable-xhdpi/
+            awesomeimage.png
+        drawable-hdpi/
+            awesomeimage.png
+        drawable-mdpi/
+            awesomeimage.png
+        drawable-ldpi/
+            awesomeimage.png
+```
+Any time you reference @drawable/awesomeimage, the system selects the appropriate bitmap based on the screen's density.
+
+## 4. Interacting with Other Apps
+### 1. Sharing Simple Data
+Just use an intent, see MainActivity.pickContact() and startNewActivity().
+
+### 2. Sharing Files
--- app/src/main/java/com/navisow/android/app_01/MainActivity.java
+++ app/src/main/java/com/navisow/android/app_01/MainActivity.java
@@ -1,15 +1,25 @@
 package com.navisow.android.app_01;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.CalendarContract;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Calendar;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.navisow.android.app_01.MESSAGE";
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     // Send Button handler
     public void sendMessage(View button) {
+/*
         Intent intent = new Intent(this, DisplayMessageActivity.class);
 
         // Get editText object
@@ -33,5 +44,90 @@ public class MainActivity extends AppCompatActivity {
         // Set the message and instantiate the DisplayMessageActivity
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+*/
+
+//        startNewActivity();
+        pickContact();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request it is that we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                // Do something with the phone number...
+            }
+        }
+    }
+
+    private void pickContact() {
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        // Show user only contacts w/ phone numbers
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE);
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    private void startNewActivity() {
+        // Build the intent for map
+        // Map point based on address
+        Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+California");
+        // Or map point based on latitude/longitude
+        // Uri location = Uri.parse("geo:37.422219,-122.08364?z=14"); // z param is zoom level
+//        Intent intent = new Intent(Intent.ACTION_VIEW, location);
+
+        // Build the intent for web
+        Uri webpage = Uri.parse("http://www.android.com");
+//        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Build the intent for email
+//        Intent intent = new Intent(Intent.ACTION_SEND);
+        // The intent does not have a URI, so declare the "text/plain" MIME type
+//        intent.setType("text/plain");
+//        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"jon@example.com"}); // recipients
+//        intent.putExtra(Intent.EXTRA_SUBJECT, "Email subject");
+//        intent.putExtra(Intent.EXTRA_TEXT, "Email message text");
+//        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse("content://path/to/email/attachment"));
+        // You can also attach multiple items by passing an ArrayList of Uris
+
+
+        // Build the intent for calendar event
+        Intent intent = new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2012, 0, 19, 7, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2012, 0, 19, 10, 30);
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis());
+        intent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis());
+        intent.putExtra(CalendarContract.Events.TITLE, "Ninja class");
+        intent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Secret dojo");
+
+        // Verify it resolves
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(intent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+
+        // Start an activity if it's safe
+        if (isIntentSafe) {
+            startActivity(intent);
+        }
     }
 }
